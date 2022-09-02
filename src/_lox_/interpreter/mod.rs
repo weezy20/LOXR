e755311d9708052ed85f813efd6b58cb69b4ff71
Abhishek Shah <abhishekshah3@gmail.com>
2022-09-02 17:51:30 +0530

    src/

--- src/_lox_/interpreter/mod.rs
+++ src/_lox_/interpreter/mod.rs
@@ -47,65 +47,7 @@ impl Interpreter {
         self.stmts.append(&mut p.parse());
         loc!(format!("Interpreter modified -> {self:?}"));
         self.interpret();
-        // Alernatively we could check
         // if self.is_repl_mode ? then for stmt in self.stmts[self.previous..].iter() { .. }
-        // This would eliminate code duplication in `interpret()` and `extend()`
-        // for stmt in self.stmts[self.previous..].iter() {
-        //     // Statements should produce no value, except ExprStmt
-        //     let val: ValueResult = match stmt {
-        //         DStmt(d) => match d {
-        //             Stmt::ExprStmt(e) | Stmt::Print(e) => e.eval(&mut self.env),
-        //             Stmt::ErrStmt { message } => {
-        //                 loc!();
-        //                 eprintln!(
-        //                     "{}{}{message}",
-        //                     "Interpreter Error: ".red(),
-        //                     "Bad statement ".yellow()
-        //                 );
-        //                 Ok(Value::Nil)
-        //             }
-        //             Stmt::Empty => Ok(Value::Nil),
-        //         },
-        //         // Declarations should produce no values
-        //         Declaration::VarDecl { name, initializer } => {
-        //             // let init_err : Option<EvalError> = None;
-        //             let val = if let Some(expr) = initializer {
-        //                 match expr.eval(&mut self.env) {
-        //                     Ok(v) => v,
-        //                     Err(eval_err) => {
-        //                         loc!();
-        //                         eprintln!("{} {eval_err}", "Interpreter Error:".red());
-        //                         continue;
-        //                     }
-        //                 }
-        //             } else {
-        //                 Value::Nil
-        //             };
-        //             println!("var {name} declared to {}", val);
-        //             self.env.define(name, val);
-        //             crate::loc!(format!("{:?}", self.env.values));
-        //             Ok(Value::Nil)
-        //         }
-        //         Declaration::ErrDecl => {
-        //             loc!();
-        //             eprintln!(
-        //                 "{}{}",
-        //                 "Interpreter Error: ".red(),
-        //                 "Variable declaration error".yellow()
-        //             );
-        //             Ok(Value::Nil)
-        //         }
-        //     };
-        //     match val {
-        //         Ok(val) => {
-        //             println!(">> {}", val);
-        //         }
-        //         Err(e) => {
-        //             loc!();
-        //             eprintln!("{} {e}", "Interpreter Error:".red());
-        //         }
-        //     };
-        // }
     }
 
     pub fn interpret(&mut self) -> () {
